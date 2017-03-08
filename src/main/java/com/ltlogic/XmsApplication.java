d3aff8f3638f2cf0bce8a566137c892c58c10be4
Jaimel Patel <jaimel@Jaimels-MacBook-Pro.local>
2017-03-07 21:35:28 -0800

    first commit

---
+++ src/main/java/com/ltlogic/XmsApplication.java
@@ -0,0 +1,12 @@
+package com.ltlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmsApplication.class, args);
+	}
+}
